d02a3bf624d63fc1ec0154b4e17701ff1ddcb4ad
Fatmirs Shurdha <57496725+Fatmirs@users.noreply.github.com>
2024-05-18 22:14:17 +0200

    Update scriptstar.js

--- js/scriptstar.js
+++ js/scriptstar.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const starContainer = document.querySelector('.star-container');
+    const starCount = 100;
 
+    for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        star.style.left = `${Math.random() * 100}vw`;
+        star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        starContainer.appendChild(star);
+    }
+});
